9ec40f2034de9174232880a7e164adca2553c9d7
ProgrammablePotato <tamas.mihelik@gmail.com>
2025-03-17 16:55:30 +0100

    base service

--- src/app/base.service.ts
+++ src/app/base.service.ts
@@ -1,13 +1,31 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { map, Observable } from 'rxjs';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BaseService {
   currentPage = "/home"
+  productsUrl = "http://localhost:3000/products/"
+  products:any = []
 
-  
-
+  constructor (private auth:AuthService, private http:HttpClient) {
+    // this.getProducts().subscribe((data:any)=>{
+    //   this.products = data
+    // })
+    this.products = this.renderProducts()
+    console.log(this.products)
+  }
+  getProducts() {
+    return this.http.get(this.productsUrl)
+  }
+  renderProducts() {
+    this.getProducts().subscribe((prods:any) => {
+      console.log(prods)
+      for () {
+        
+      }
+    }) 
+  }
 }
