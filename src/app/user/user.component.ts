089ab8e8db8d05a1fc9779740161f19b95d823c4
Elydra5 <kliberbenedek@gmail.com>
2025-03-19 19:52:00 +0100

    felhasználói felület

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-user',
@@ -6,5 +8,46 @@ import { Component } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
+  isUserAdmin: boolean = false
+  passwordVisibility = "password"
 
+  ngOnInit(): void{
+    this.auth.getIsAdmin().subscribe((isAdmin) => {
+      this.isUserAdmin = isAdmin
+    })
+  }
+
+  loggedUser:any
+
+  constructor(private auth:AuthService, private route: ActivatedRoute, private router:Router){
+    this.auth.getLoggedUser().subscribe((user) => {
+      this.loggedUser = user
+    })
+  }
+
+  saveChanges(){
+    this.auth.updateUser(this.loggedUser.displayName, this.loggedUser.phoneNumber, this.loggedUser.email, this.loggedUser.password)?.subscribe(
+      (res) => {
+        if(this.loggedUser.email != this.loggedUser.email){
+          alert("Please login again with your new email!")
+          this.auth.logout()
+          this.router.navigate(['/login'])
+        }
+        else if(this.loggedUser.password){
+          alert("Please login again with your new password!")
+          this.auth.logout()
+          this.router.navigate(['/login'])
+        }
+        console.log("Update", res)
+      }
+    )
+  }
+
+  changeVisibility() {
+    if (this.passwordVisibility === "password") {
+      this.passwordVisibility = "text"
+    } else {
+      this.passwordVisibility = "password"
+    }
+  }
 }
