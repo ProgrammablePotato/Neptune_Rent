821fd5cf01eb2ab27e4817026cd3f653579b6fd1
Turcsik Daniel <turcsikdani69@gmail.com>
2025-03-20 12:07:21 +0100

    start batch file for testing purposes

---
+++ neptune.bat
@@ -0,0 +1,7 @@
+cd "C:\Users\Diak\Desktop\neptune\NeptuneRent"
+START code .
+START ng serve -o
+cd "C:\Users\Diak\Desktop\neptune\NeptuneRent\backend"
+START npm start
+cd "C:\Users\Diak\Desktop\neptune\NeptuneRent\functions"
+START npm run serve
