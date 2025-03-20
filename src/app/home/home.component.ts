597c1da76e10dab95a4b99fcb96b3a4105e81f5d
Matyas Zsuzsa Krisztina <2reanimate@gmail.com>
2025-03-20 11:35:50 +0100

    home kiegészítése

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { NewsService } from '../news.service';
+import { BaseService } from '../base.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +9,47 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+
+  allNews:any = []
+  currentSlideIndex: number = 0
+  products:any = []
+
+  constructor(private news:NewsService, private base:BaseService, private router:Router){
+    this.base.currentPage = this.router.url
+    this.getNews()
+    this.getProducts()
+  }
+
+  async getNews(){
+    this.news.getTechNews().subscribe((data) => {
+      this.allNews = data.articles.slice(0, 5)
+      console.log(this.allNews)
+    })
+  }
+
+  prevSlide() {
+    if (this.currentSlideIndex > 0) {
+      this.currentSlideIndex--
+      console.log(this.currentSlideIndex)
+    } else {
+      this.currentSlideIndex = this.allNews.length - 1
+      console.log(this.currentSlideIndex)
+    }
+  }
+
+  nextSlide() {
+    if (this.currentSlideIndex < this.allNews.length - 1) {
+      this.currentSlideIndex++
+      console.log(this.currentSlideIndex)
+    } else {
+      this.currentSlideIndex = 0
+      console.log(this.currentSlideIndex)
+    }
+  }
+
+  async getProducts() {
+    this.products = await this.base.getProducts()
+    this.base.roundPrices()
+  }
 
 }
