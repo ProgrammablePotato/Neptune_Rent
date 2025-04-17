4bf41d2e42cb8df13a9860e34f6a06df54e15f6d
ProgrammablePotato <tamas.mihelik@gmail.com>
2025-04-17 12:37:41 +0200

    thththt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,18 +14,6 @@ export class AppComponent implements OnInit {
     initFlowbite();
   }
 }
-export async function newsCarouselSlide(news:any[]) {
-    let activeNews:any
-    // let nextNews:any
-    let newsCount = 0
-    var activeItem = document.getElementById("active-carousel-item")
-    // var inactiveItem = document.getElementById("inactive-carousel-item")
-    while (true) {
-      activeNews = news[newsCount]
-      delay(5000)
-      newsCount++
-    }
-  }
 export function dropdownCollapse(id:string) {
     var dropdown:any = document.getElementById(`neptune-dropdown-${id}`)
     dropdown.style.display = "none"
